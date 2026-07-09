55332211cf65083a0ea624271d26f402ca1ac92c
popi753 <luka.popi.919@gmail.com>
2026-07-09 18:25:08 +0400

    chore: update ESLint parser options to include node tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,7 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.app.json'],
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
