048f3826725411978ce1191a9777310dc22b08ee
popi753 <luka.popi.919@gmail.com>
2026-07-13 09:04:52 +0400

    feat: add winners page slice and integrate with garage store

---
+++ src/store/slices/winnersPageSlice.ts
@@ -0,0 +1,31 @@
+import type { StateCreator } from 'zustand'
+import type { SortOrder, SortField } from '@/types';
+
+export type WinnersPageState = {
+    winnersPage: number;
+    setWinnersPage: (newPage: number) => void;
+
+    sort: SortField;
+    setSort: (newSort: SortField) => void;
+
+    order: SortOrder;
+
+    totalWinnersCount: number;
+    setTotalWinnersCount: (newTotalCount: number) => void;
+}
+
+export const createWinnersPageSlice: StateCreator<WinnersPageState> = (set, get) => ({
+    winnersPage: 1,
+    setWinnersPage: (newPage) => set({ winnersPage: newPage }),
+    sort: "",
+    setSort: (newSort) => {
+        const currentOrder = get().order;
+        set({
+            sort:
+                newSort, order: currentOrder === "ASC" ? "DESC" : "ASC"
+        });
+    },
+    order: 'ASC',
+    totalWinnersCount: 0,
+    setTotalWinnersCount: (newTotalCount) => set({ totalWinnersCount: newTotalCount }),
+})
--- src/store/useGarageStore.ts
+++ src/store/useGarageStore.ts
@@ -3,11 +3,12 @@ import { createGaragePageSlice, type GaragePageState } from './slices/garagePage
 import { createCarCreationSlice, type CarSliceState } from './slices/carCreationSlice';
 import { createEditCarSlice, type EditCarSliceState } from './slices/editCarSlice';
 import { createRacingSlice, type RacingSliceState } from './slices/racingSlice';
+import { createWinnersPageSlice, type WinnersPageState } from './slices/winnersPageSlice';
 
-export const useGarageStore = create<GaragePageState & CarSliceState & EditCarSliceState & RacingSliceState>((...rest) => ({
+export const useGarageStore = create<GaragePageState & CarSliceState & EditCarSliceState & RacingSliceState & WinnersPageState>((...rest) => ({
     ...createGaragePageSlice(...rest),
     ...createCarCreationSlice(...rest),
     ...createEditCarSlice(...rest),
     ...createRacingSlice(...rest),
-
+    ...createWinnersPageSlice(...rest),
 }))
